6c3663c91577cfb62fbeccaf54df583a0b28bb01
Ivan Zou <ivanzou@seas.upenn.edu>
2025-04-09 14:35:00 -0400

    created orb spawning system. Spawn  in front of player still in progress

---
+++ Assets/Game Assets/Scripts/OrbSpawner.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class OrbSpawner : MonoBehaviour {
+    [Header("Orb Prefabs")]
+    public GameObject standardOrb;
+    public GameObject pulseOrb;
+
+    [Header("Spawn Options")]
+    public bool spawnStandard = true;
+    public bool spawnPulse = false;
+
+    public bool spawnInPlace = true;
+    public bool spawnInFrontOfPlayer = false;
+    public Transform playerCamera;
+    public float spawnGap = 0.25f;
+
+    [Header("If spawnInPlace and InFrontOfPlayer is false, spawn orbs randomly in area")]
+    public float spawnAreaSizeX = 5.0f;
+    public float spawnAreaSizeZ = 5.0f;
+    public float spawnHeightY = 1.0f;
+
+    [Header("Respawn Delay")]
+    public float respawnDelay = 2.0f;
+
+    private Dictionary<string, GameObject> spawnedOrbs = new Dictionary<string, GameObject>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        TrySpawnAll();
+    }
+    void TrySpawnAll() {
+        int spawnIndex = 0;
+
+        if (spawnStandard) TrySpawnOrb("Standard", standardOrb, spawnIndex++);
+        if (spawnPulse) TrySpawnOrb("Pulse", pulseOrb, spawnIndex++);
+    }
+
+    void TrySpawnOrb(string orbType, GameObject prefab, int index) {
+        if (spawnedOrbs.ContainsKey(orbType) && spawnedOrbs[orbType] != null) return;
+
+        Vector3 spawnPos = transform.position;
+
+        if (spawnInPlace) {
+            spawnPos += new Vector3(index * spawnGap, 0, 0);
+
+        } else if (spawnInFrontOfPlayer) {
+            Vector3 forward = playerCamera.forward;
+            forward.y = 0;
+            forward.Normalize();
+
+            Vector3 baseSpawnPos = playerCamera.position + forward * 0.75f;
+
+            spawnPos = baseSpawnPos;
+            Vector3 right = playerCamera.right;
+            spawnPos += right.normalized * spawnGap * index;
+        } else {
+            float randX = Random.Range(-spawnAreaSizeX / 2.0f, spawnAreaSizeX / 2.0f);
+            float randZ = Random.Range(-spawnAreaSizeZ / 2.0f, spawnAreaSizeZ / 2.0f);
+            spawnPos += new Vector3(randX, 0f, randZ);
+        }
+
+        spawnPos.y = spawnHeightY;
+
+        GameObject orb = Instantiate(prefab, spawnPos, Quaternion.identity);
+        spawnedOrbs[orbType] = orb;
+
+        // Subscribe to grab event
+        XRGrabInteractable grab = orb.GetComponent<XRGrabInteractable>();
+        if (grab != null) {
+            grab.selectEntered.AddListener((args) => OnOrbGrabbed(orbType));
+        }
+    }
+
+    void OnOrbGrabbed(string orbType) {
+        if (spawnedOrbs.ContainsKey(orbType)) {
+            // Remove reference
+            spawnedOrbs[orbType] = null;
+        }
+
+        // Respawn after delay
+        StartCoroutine(RespawnAfterDelay(orbType));
+    }
+
+    IEnumerator RespawnAfterDelay(string orbType) {
+        yield return new WaitForSeconds(respawnDelay);
+
+        int spawnIndex = 0;
+        if (orbType == "Standard") TrySpawnOrb("Standard", standardOrb, spawnIndex);
+        else if (orbType == "Pulse") TrySpawnOrb("Pulse", pulseOrb, spawnIndex + (spawnStandard ? 1 : 0));
+    }
+}
