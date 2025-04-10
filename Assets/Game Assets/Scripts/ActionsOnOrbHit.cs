113ef0cbbd13b98dca13cb1cc79035af057d3c5e
Ivan Zou <ivanzou@seas.upenn.edu>
2025-04-09 22:12:02 -0400

    added visual and audio feedback on target hit

--- Assets/Game Assets/Scripts/ActionsOnOrbHit.cs
+++ Assets/Game Assets/Scripts/ActionsOnOrbHit.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class ActionsOnOrbHit : MonoBehaviour {
     // Set the value of this target/score zone in its prefab
     public float pointValue = 0;
+
+    public GameObject hitExplosion;
+    public AudioClip hitSound;
     private void OnCollisionEnter(Collision collision) {
         Collider collider = collision.collider;
 
@@ -12,8 +15,13 @@ public class ActionsOnOrbHit : MonoBehaviour {
             int bounceCount = bounceCounter.bounceCount;
             // Destroy the target/score zone if the orb bounced at least once
             if (bounceCount > 0) {
+                // Play damaged sound
+                AudioSource.PlayClipAtPoint(hitSound, gameObject.transform.position);
+                // Create explosion particles
+                Instantiate(hitExplosion, gameObject.transform.position, Quaternion.identity);
+                // Destroy the target/score zone
                 Destroy(gameObject);
-                // Destory the orb
+                // Destroy the orb
                 Destroy(bounceCounter.gameObject);
                 // Do stuff with pointValue
             }
